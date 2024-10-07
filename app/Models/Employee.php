97f88072fbd456f06d42e39b414b5c20429c90c1
katherinekhine <khaingkatherine@gmail.com>
2024-10-07 14:42:43 +0630

    update Employee.php

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Employee extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'phone',
+        'postiton',
+    ];
 }
