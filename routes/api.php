d9b736628d8f0963b4267fe5d404bb44d72177da
katherinekhine <khaingkatherine@gmail.com>
2024-10-08 15:10:10 +0630

    nothing change just delete space

--- routes/api.php
+++ routes/api.php
@@ -5,12 +5,7 @@ use GraphQL\GraphQL as GraphQLGraphQL;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 // use Nuwave\Lighthouse\GraphQL;
-<<<<<<< HEAD
-use Nuwave\Lighthouse\Support\Facades\GraphQL;
-=======
 // use Nuwave\Lighthouse\Support\Facades\GraphQL;
->>>>>>> c0c7afd (update api.php, app.php (after rollback))
-
 
 Route::get('/user', function (Request $request) {
     return $request->user();
