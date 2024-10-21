4b8bcfe2e18bbde69869f5fb80a7e6b3194c92f2
katherinekhine <khaingkatherine@gmail.com>
2024-10-21 14:10:34 +0630

    update EmployeeImport.php

--- app/Imports/EmployeeImport.php
+++ app/Imports/EmployeeImport.php
@@ -22,15 +22,26 @@ class EmployeeImport implements ToCollection, ToModel
     {
         $this->current++;
         if ($this->current > 1) {
-            $count = Employee::where('email', '=', $row[1])->count();
-            if (empty($count)) {
-                $employee = new Employee();
-                $employee->name = $row[1];
-                $employee->email = $row[2];
-                $employee->phone = $row[3];
-                $employee->postiton = $row[4];
-                $employee->save();
-            }
+            // $count = Employee::where('email', '=', $row[2])->count();
+            // if (empty($count)) {
+            //     $employee = new Employee();
+            //     $employee->id = $row[0];
+            //     $employee->name = $row[1];
+            //     $employee->email = $row[2];
+            //     $employee->phone = $row[3];
+            //     $employee->postiton = $row[4];
+            //     $employee->save();
+            // }
+
+            Employee::updateOrCreate(
+                ['email' => $row[2]],
+                [
+                    'id' => $row[0],
+                    'name' => $row[1],
+                    'phone' => $row[3],
+                    'postiton' => $row[4],
+                ]
+            );
         }
     }
 }
